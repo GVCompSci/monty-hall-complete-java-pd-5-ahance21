232c247216f6f6c869d9b828258e2ed1a578ffae
ahance21 <42975655+ahance21@users.noreply.github.com>
2019-01-30 09:34:48 -0500

    Add files via upload

--- src/MontyHall3.java
+++ src/MontyHall3.java
@@ -8,26 +8,43 @@ public class MontyHall3 {
 	{
 		Scanner keyboard = new Scanner(System.in);
 		Random rand = new Random();
-		int carDoor = rand.nextInt(3) + 1;
+		
+		int door1 = rand.nextInt(3) + 1;
 		int win = 0;
 		System.out.print("Please enter the number of times you would like to roll ");
 		String rollStr = keyboard.nextLine();
 		int roll = Integer.parseInt(rollStr);
 		{
-		if (roll < 10 || roll > 10000)
-		System.out.print("Enter a nuber between 10 and 10000 ");
+			if (roll < 10 || roll > 10000)
+			System.out.print("Enter a nuber between 10 and 10000 ");
 		}
-	    if (roll >= 10 || roll <= 10000)
-		System.out.print("Would you like to switch doors? ");
-        String doorswitch = keyboard.nextLine();
-        {
-        if (doorswitch.equals("no"))
-        for(int i =0; i < roll; i++)
-        while (roll == carDoor)
-        win++;
+	    	if (roll >= 10 || roll <= 10000)
+	    	System.out.print("Would you like to switch doors? ");
+	    	String doorswitch = keyboard.nextLine();
+            { 
         
+            if (doorswitch.equals("no"))
+            for(int i =0; i < roll; i++)
+            {
+            	int carDoor1 = rand.nextInt(3) + 1;
+                if (door1 == carDoor1) {
+                win++;
+                System.out.print("\nYou won " + win);
+            }
+            }
         
-        } 
+        }   
+            if (doorswitch.equals("yes"))
+            for (int i =0; i < roll; i++)
+        {       
+            int door2 = rand.nextInt(3) + 1;
+            int carDoor2 = rand.nextInt(3) + 1;
+            if (door2 == carDoor2) {
+            win++;   
+            System.out.print("\nYou won " + win);
+           }
+       
+      
+        }
 	}
-
 }
